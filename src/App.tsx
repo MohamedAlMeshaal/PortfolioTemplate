2bd050e20f7c19c086665a015bbc2a90863d0b8f
MohamedAlMeshaal <mhmsaed200213@gmail.com>
2025-12-10 01:06:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+
+import Index from "./pages/Index";
 import ProjectDetails from "./pages/ProjectDetails";
+import NotFound from "./pages/NotFound";
 
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
         <Routes>
           <Route path="/" element={<Index />} />
-<Route path="/projects/:slug" element={<ProjectDetails />} />
+          <Route path="/projects/:slug" element={<ProjectDetails />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
