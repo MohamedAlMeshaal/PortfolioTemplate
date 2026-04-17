548668cb0bfe4bf655d7bb3fc1d1cdbe01982ca4
Mohamed Saed <mhmsaed200213@gmail.com>
2026-04-17 04:14:42 +0300

    add Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
