86014944e6a8c9781e4078e7d545c299148e99d0
Mohamed Saed <mhmsaed200213@gmail.com>
2026-04-18 19:59:27 +0300

    add viewport meta tag for mobile

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { inject } from "@vercel/analytics";
-
-inject();
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>
+);
